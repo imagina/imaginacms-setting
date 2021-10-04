552fe4a7613eb42be48caa2a722759459dea709f
Juan Escalante <jcec007@gmail.com>
2021-10-04 18:58:23 -0500

    +Update: remove settings and permissions from the published config

--- Providers/SettingServiceProvider.php
+++ Providers/SettingServiceProvider.php
@@ -20,88 +20,89 @@ use Modules\Setting\Support\Settings;
 
 class SettingServiceProvider extends ServiceProvider
 {
-    use CanPublishConfiguration, CanGetSidebarClassForModule;
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerBindings();
-
-        $this->app->singleton('setting.settings', function ($app) {
-            return new Settings($app[SettingRepository::class]);
-        });
-
-        $this->app->booting(function () {
-            $loader = AliasLoader::getInstance();
-            $loader->alias('Settings', SettingsFacade::class);
-        });
-
-        $this->app->bind('setting.setting.directive', function () {
-            return new SettingDirective();
-        });
-
-        $this->app['events']->listen(
-            BuildingSidebar::class,
-            $this->getSidebarClassForModule('setting', RegisterSettingSidebar::class)
-        );
-
-        $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
-            $event->load('settings', Arr::dot(trans('setting::settings')));
-        });
+  use CanPublishConfiguration, CanGetSidebarClassForModule;
+  
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = false;
+  
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->registerBindings();
+    
+    $this->app->singleton('setting.settings', function ($app) {
+      return new Settings($app[SettingRepository::class]);
+    });
+    
+    $this->app->booting(function () {
+      $loader = AliasLoader::getInstance();
+      $loader->alias('Settings', SettingsFacade::class);
+    });
+    
+    $this->app->bind('setting.setting.directive', function () {
+      return new SettingDirective();
+    });
+    
+    $this->app['events']->listen(
+      BuildingSidebar::class,
+      $this->getSidebarClassForModule('setting', RegisterSettingSidebar::class)
+    );
+    
+    $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
+      $event->load('settings', Arr::dot(trans('setting::settings')));
+    });
+  }
+  
+  public function boot()
+  {
+    $this->mergeConfigFrom($this->getModuleConfigFilePath('setting', 'permissions'), "asgard.setting.permissions");
+    $this->publishConfig('setting', 'config');
+    $this->registerBladeTags();
+    $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+  }
+  
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return [];
+  }
+  
+  private function registerBindings()
+  {
+    $this->app->bind(SettingRepository::class, function () {
+      $repository = new EloquentSettingRepository(new Setting());
+      
+      if (!config('app.cache')) {
+        return $repository;
+      }
+      
+      return new CacheSettingDecorator($repository);
+    });
+    $this->app->bind(
+      \Modules\Setting\Contracts\Setting::class,
+      Settings::class
+    );
+  }
+  
+  private function registerBladeTags()
+  {
+    if (app()->environment() === 'testing') {
+      return;
     }
-
-    public function boot()
-    {
-        $this->publishConfig('setting', 'permissions');
-        $this->publishConfig('setting', 'config');
-        $this->registerBladeTags();
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [];
-    }
-
-    private function registerBindings()
-    {
-        $this->app->bind(SettingRepository::class, function () {
-            $repository = new EloquentSettingRepository(new Setting());
-
-            if (! config('app.cache')) {
-                return $repository;
-            }
-
-            return new CacheSettingDecorator($repository);
-        });
-        $this->app->bind(
-            \Modules\Setting\Contracts\Setting::class,
-            Settings::class
-        );
-    }
-
-    private function registerBladeTags()
-    {
-        if (app()->environment() === 'testing') {
-            return;
-        }
-        $this->app['blade.compiler']->directive('setting', function ($value) {
-            return "<?php echo SettingDirective::show([$value]); ?>";
-        });
-    }
+    $this->app['blade.compiler']->directive('setting', function ($value) {
+      return "<?php echo SettingDirective::show([$value]); ?>";
+    });
+  }
 }
