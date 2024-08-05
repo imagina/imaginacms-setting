f6fea63e28c91d2dddb59dfb6d1f041fe6a020bb
msolanogithub <msolano@imaginacolombia.com>
2024-08-05 18:05:23 -0500

    instance the data for cacheClearable trait for CDN provider in the SETTING entity

--- Entities/Setting.php
+++ Entities/Setting.php
@@ -6,10 +6,11 @@ use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Media\Support\Traits\MediaRelation;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use Modules\Core\Icrud\Traits\HasCacheClearable;
 
 class Setting extends Model
 {
-    use Translatable, MediaRelation, BelongsToTenant;
+    use Translatable, MediaRelation, BelongsToTenant, HasCacheClearable;
 
     public $translatedAttributes = ['value', 'description'];
     protected $fillable = ['name', 'value', 'description', 'isTranslatable', 'plainValue','organization_id'];
@@ -21,18 +22,27 @@ class Setting extends Model
 
         return is_array($value) && (isset($value['medias_single']) || isset($value['medias_multi']));
     }
-    
+
     public function ownHasTranslation($locale){
-  
+
       return \Cache::store(config("cache.default"))->tags("setting.settings")->remember('own_has_translation_setting_'.$this->id.'_'.$locale , 60, function () use($locale) {
         return $this->hasTranslation($locale);
       });
     }
-    
+
     public function getValueByLocale($locale){
-  
+
       return \Cache::store(config("cache.default"))->tags("setting.settings")->remember('translate_by_locale_setting_'.$this->id.'_'.$locale , 60, function () use($locale) {
         return $this->translate($locale)->value;
       });
     }
+
+  public function getCacheClearableData()
+  {
+    return [
+      'cdn' => [
+        config("app.url")
+      ]
+    ];
+  }
 }
