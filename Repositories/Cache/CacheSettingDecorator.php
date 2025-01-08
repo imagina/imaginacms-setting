9fd496cb729b329366fe83dbfa31ca8d0e46c7ce
William <williamverde10@gmail.com>
2025-01-08 14:56:35 -0400

    Added: params to findByName | Cache Decorator

--- Repositories/Cache/CacheSettingDecorator.php
+++ Repositories/Cache/CacheSettingDecorator.php
@@ -35,8 +35,8 @@ class CacheSettingDecorator extends BaseCacheDecorator implements SettingReposit
      */
     public function findByName($settingName, $central = false, $organizationId = null)
     {
-        $settingValue = $this->remember(function () use ($settingName) {
-            return $this->repository->findByName($settingName) ?? $settingName.'___NULL';
+        $settingValue = $this->remember(function () use ($settingName,$central,$organizationId) {
+            return $this->repository->findByName($settingName,$central,$organizationId) ?? $settingName.'___NULL';
         });
 
         if($settingValue===$settingName.'___NULL') $settingValue = null;
